34da1dad3e9686dd99ce2df9d672e0ed104ea5b6
Санжар Алим <alim@kolesa.kz>
2021-05-01 19:26:15 +0600

    chore: change pod ios target version

--- Example/AwesomeTooltip/ViewController.swift
+++ Example/AwesomeTooltip/ViewController.swift
@@ -7,18 +7,14 @@
 //
 
 import UIKit
+import AwesomeTooltip
 
 class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+//        let viewController = TooltipViewController(
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
-
 }
 
